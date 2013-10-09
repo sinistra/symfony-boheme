a985f4d2b5ac09bb24ecaa57773b0cb1c2b4bd2c
paul <paul@paul-VirtualBox>
2013-10-10 09:21:51 +1100

    add display actions to default controller

--- src/Boheme/CafeBundle/Controller/DefaultController.php
+++ src/Boheme/CafeBundle/Controller/DefaultController.php
@@ -8,9 +8,29 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $name = '
-            Darren';
+        $name = 'Darren';
         return $this->render('BohemeCafeBundle:Default:index.html.twig',
+                array('name' => $name));
+    }
+
+    public function mealAction()
+    {
+        $name = 'Darren';
+        return $this->render('BohemeCafeBundle:Default:meal.html.twig',
+                array('name' => $name));
+    }
+
+    public function wineAction()
+    {
+        $name = 'Darren';
+        return $this->render('BohemeCafeBundle:Default:wine.html.twig',
+                array('name' => $name));
+    }
+
+    public function contactAction()
+    {
+        $name = 'Darren';
+        return $this->render('BohemeCafeBundle:Default:contact.html.twig',
                 array('name' => $name));
     }
 }
