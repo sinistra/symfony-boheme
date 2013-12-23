33cc3c0aae0a11f72322126a79e68a26b682c7c6
Paul Taylor <paul@nms.com.au>
2013-12-24 08:46:55 +1100

    add wine type table

---
+++ src/Boheme/CafeBundle/Entity/Winetype.php
@@ -0,0 +1,245 @@
+<?php
+// file: src/Boheme/CafeBundle/Entity/Winetype.php
+
+namespace Boheme\CafeBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo; // gedmo annotations
+use Doctrine\ORM\Mapping as ORM; // doctrine orm annotations
+use Symfony\Component\Validator\ExecutionContextInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="Boheme\CafeBundle\Entity\WinetypeRepository")
+ * @ORM\Table(name="winetypes")
+ */
+
+class Winetype {
+
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(length=64, type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Wine", mappedBy="type")
+     */
+    private $wines;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var string $createdBy
+     *
+     * @Gedmo\Blameable(on="create")
+     * @ORM\Column(length=25, type="string")
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\Column(name="updated", type="datetime")
+     * @Gedmo\Timestampable(on="update")
+     */
+    private $updated;
+
+    /**
+     * @var string $updatedBy
+     *
+     * @Gedmo\Blameable(on="update")
+     * @ORM\Column(length=25, type="string")
+     */
+    private $updatedBy;
+
+    public function isExpireValid(ExecutionContextInterface $context)
+    {
+        // check if the expiry date is before the publish date
+        if ($this->expire < $this->publish) {
+            $context->addViolationAt('expire', 'The Expiry date must not be less than the Publish date!', array(), null);
+        }
+    }
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->wines = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Wineregion
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Wineregion
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     * @return Wineregion
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Wineregion
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param string $updatedBy
+     * @return Wineregion
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return string
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Add wines
+     *
+     * @param \Boheme\CafeBundle\Entity\Wine $wines
+     * @return Wineregion
+     */
+    public function addWine(\Boheme\CafeBundle\Entity\Wine $wines)
+    {
+        $this->wines[] = $wines;
+
+        return $this;
+    }
+
+    /**
+     * Remove wines
+     *
+     * @param \Boheme\CafeBundle\Entity\Wine $wines
+     */
+    public function removeWine(\Boheme\CafeBundle\Entity\Wine $wines)
+    {
+        $this->wines->removeElement($wines);
+    }
+
+    /**
+     * Get wines
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getWines()
+    {
+        return $this->wines;
+    }
+
+    /**
+     * toString
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
